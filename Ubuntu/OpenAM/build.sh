99a27789c0603507308bee104a0a1fa072c44e53
Matt Choinski <matt@choinski.net>
2015-10-05 10:00:39 -0400

    Updated scripts and the ignore file.

--- Ubuntu/OpenAM/build.sh
+++ Ubuntu/OpenAM/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: Investigate to be sure a non-root account is being used.
+#su - vagrant
+
 wget https://github.com/mattchoinski/Vagrant-Scripts/raw/master/Ubuntu/OpenAM/external_library/authapi.jar
 mv authapi.jar ~/.m2/repository/external/authapi/2005-08-12/authapi-2005-08-12.jar
 
@@ -7,4 +10,4 @@ svn checkout https://svn.forgerock.org/openam/trunk/ openam_svn
 mkdir -p ./openam_svn/openam/target/legal-notices
 
 cd ./openam_svn/openam
-maven install
+mvn install
