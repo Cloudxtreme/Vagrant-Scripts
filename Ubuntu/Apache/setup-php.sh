1c2a60c5067e8bce521cebff415037a328670cd9
Matt Choinski <matt@choinski.net>
2016-02-01 06:47:39 -0500

    Updated various scripts, but still need to finish the MongoDB-related scripts.

--- Ubuntu/Apache/setup-php.sh
+++ Ubuntu/Apache/setup-php.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -qq libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-mcrypt
+echo "Installing PHP..."
+{
+	sudo apt-get update
+	sudo apt-get install -qq libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-mcrypt
 
-sudo service apache2 restart
+	sudo service apache2 restart
+} &> /dev/null
+echo "...done."
--- Ubuntu/MongoDB/setup-single.sh
+++ Ubuntu/MongoDB/setup-single.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-sudo apt-get install -y mongodb-org
+echo "Installing MongoDB 3.2..."
+{
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+	echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+	sudo apt-get update
+	sudo apt-get install -qq mongodb-org
 
-sudo service mongod start
-
-# How to install the PHP Driver;
-#sudo pecl install mongodb
-#sudo echo "extension=mongodb.so" >> /etc/php5/apache2/php.ini
+	sudo mv /tmp/mongodb-disable-hugepages /etc/init.d/disable-transparent-hugepages
+	sudo chmod 755 /etc/init.d/disable-transparent-hugepages
+	sudo update-rc.d disable-transparent-hugepages defaults
+} &> /dev/null
+echo "...done."
